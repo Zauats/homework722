822992d177cdd3ca2c9d184735d6983f81d46086
Zauats <cfif12342002@yandex.ru>
2019-07-18 14:58:31 +0300

    cd

--- app/src/main/java/com/example/homework722/MainActivity.java
+++ app/src/main/java/com/example/homework722/MainActivity.java
@@ -13,6 +13,7 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,45 +21,70 @@ public class MainActivity extends AppCompatActivity {
     EditText message;
     Intent newCall;
     Uri uri;
-
+    public int MY_PERMISSIONS_REQUEST_CALL_PHONE = 100;
+    public int MY_PERMISSIONS_REQUEST_SEND_MESSAGE = 101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         phone = findViewById(R.id.phoneText);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                call();
+            }
+        });
 
     }
 
-    public void call(View view){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED){
+    public void call(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED)
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE}, 100);
-            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED)) {
-                uri = Uri.parse("tel:" + phone.getText().toString());
-                newCall = new Intent(Intent.ACTION_CALL, uri);
-                startActivity(newCall);
-            }
-        }else{
+        else{
             uri = Uri.parse("tel:" + phone.getText().toString());
             newCall = new Intent(Intent.ACTION_CALL, uri);
             startActivity(newCall);
         }
     }
 
-    public void sendMessage(View view){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED){
+    public void sendMessage(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED)
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS}, 101);
-            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED)){
-                SmsManager smgr = SmsManager.getDefault();
-                uri = Uri.parse("tel:" + phone.getText().toString());
-                smgr.sendTextMessage(phone.getText().toString(),null,message.getText().toString(),null,null);
-            }
-        }
         else{
             SmsManager smgr = SmsManager.getDefault();
             uri = Uri.parse("tel:" + phone.getText().toString());
             smgr.sendTextMessage(phone.getText().toString(),null,message.getText().toString(),null,null);
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 100: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call();
+                } else {
+                    Toast.makeText(this, "Ошибочка", Toast.LENGTH_LONG);
+                }
+            }
+            case 101: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    sendMessage();
+                } else {
+                    Toast.makeText(this, "Ошибочка", Toast.LENGTH_LONG);
+                }
+            }
+        }
+    }
+
+
 
 
 
